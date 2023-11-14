cd886935a5d7d09e70d90e3927712122a3ad419a
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2023-11-14 23:23:52 +0700

    Update evaluate.py

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
-def score(labels, preds):
+def cal_score(labels, preds):
     cm = confusion_matrix(labels, preds)
     accuracy = accuracy_score(labels, preds)
     f1 = f1_score(labels, preds, average = 'macro', zero_division=1)
