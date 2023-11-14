a57c0313b0d93dce07ed5e85c6152cd109d36781
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2023-11-14 12:58:56 +0700

    Create model.py

---
+++ model/model.py
@@ -0,0 +1,31 @@
+from typing import List, Dict, Optional
+import torch
+import torch.nn as nn
+
+from lenet import LeNet
+from googlenet import GoogLeNet
+from resnet18 import ResNet_18
+from resnet50 import ResNet_50
+
+class CNN_Model(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        if config['model'] == 'lenet':
+            self.cnn = LeNet(config)
+        elif config['model'] == 'googlenet':
+            self.cnn = GoogLeNet(config)
+        elif config['model'] == 'resnet18':
+            self.cnn = ResNet_18(config)
+        else:
+            self.cnn = ResNet_50(config)
+
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, imgs, labels=None):
+        if labels is not None:
+            logits = self.cnn(imgs)
+            loss = self.loss_fn(logits, labels)
+            return logits, loss
+        else:
+            logits = self.mlp(imgs)
+            return logits
