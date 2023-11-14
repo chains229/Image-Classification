6840a24ffc09efc80178e0fbf8ca2484fd13314d
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2023-11-14 22:03:40 +0700

    Update data_loader.py

--- data_utils/data_loader.py
+++ data_utils/data_loader.py
@@ -20,7 +20,7 @@ mnist_dataset_test = datasets.MNIST(root='./data', train=False, download=True,
     
 # CIFAR10
 cifar_10_dataset_train = datasets.CIFAR10(
-                root=data_root, train=True, download=True,
+                root='./data', train=True, download=True,
                 transform=transforms.Compose([
                     transforms.Pad(4),
                     transforms.RandomCrop(32),
@@ -30,7 +30,7 @@ cifar_10_dataset_train = datasets.CIFAR10(
                 ]))
 
 cifar_10_dataset_test = datasets.CIFAR10(
-                root=data_root, train=False, download=True,
+                root='./data', train=False, download=True,
                 transform=transforms.Compose([
                     transforms.ToTensor(),
                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
