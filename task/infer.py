3019230e9dcfca8a7be4a7bd2c1348c6be5b62ad
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2023-11-14 12:54:48 +0700

    Create infer.py

---
+++ task/infer.py
@@ -0,0 +1,42 @@
+    
+import os
+import torch
+from data_utils.load_data import getDataloader
+from evaluate.evaluate import score
+from model.model import CNN_Model
+from tqdm import tqdm
+from sklearn.metrics import classification_report
+
+class Predicting():
+    def __init__(self,config):
+        self.save_path=config['save_path']
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = CNN_Model(config).to(self.device)
+        self.test_loader = getDataloader(config).get_test()
+    
+    def main(self):
+        if os.path.exists(os.path.join(self.save_path, 'best_model.pth')):
+            checkpoint = torch.load(os.path.join(self.save_path, 'best_model.pth'), map_location=self.device)
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+        else:
+            print('havent trained yet...')
+
+
+        self.model.eval()
+        true_labels = []
+        pred_labels = []
+
+        with torch.no_grad():
+            for batch, (it,item) in enumerate(tqdm(self.test_loader)):
+                images, labels = item['image'].to(self.device), item['label'].to(self.device)
+                logits = self.model(images)
+                true_labels.extend(labels.cpu().numpy())
+                pred_labels.extend(logits.argmax(-1).cpu().numpy())
+        
+        cm, test_acc, test_f1, test_precision, test_recall = score(true_labels,pred_labels)
+        print(f"test acc: {test_acc:.4f} | test f1: {test_f1:.4f} | test precision: {test_precision:.4f} | test recall: {test_recall:.4f}")
+        print("confusion matrix:\n")
+        print(cm)
+        print('classification report:\n')
+        print(classification_report(true_labels, pred_labels))
+        
