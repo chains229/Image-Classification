c3c7b13b34abb065526a888bb5f4213e8dae2a28
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2023-11-14 13:02:34 +0700

    Create evaluate.py

---
+++ evaluate/evaluate.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+
+def score(labels, preds):
+    cm = confusion_matrix(labels, preds)
+    accuracy = accuracy_score(labels, preds)
+    f1 = f1_score(labels, preds, average = 'macro', zero_division=1)
+    precision = precision_score(labels, preds, average = 'macro', zero_division=1)
+    recall = recall_score(labels, preds, average = 'macro', zero_division=1)
+    return cm, accuracy, f1, precision, recall
