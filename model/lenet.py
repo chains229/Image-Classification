f0bb093e167ca87621b9edb485a0e4623323ad2c
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2023-11-15 15:05:53 +0700

    Update lenet.py

--- model/lenet.py
+++ model/lenet.py
@@ -7,17 +7,19 @@ class LeNet(nn.Module):
         super(LeNet, self).__init__()
         self.conv1 = nn.Conv2d(config['input_channel'], 6, kernel_size=5)
         self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.avg1 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.avg2 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, config['num_classes'])  # Output layer with 10 units for 10 classes
+        self.fc3 = nn.Linear(84, config['num_classes']) 
 
     def forward(self, x):
         x = nn.functional.relu(self.conv1(x))
-        x = nn.functional.max_pool2d(x, 2)
+        x = self.avg1(x)
         x = nn.functional.relu(self.conv2(x))
-        x = nn.functional.max_pool2d(x, 2)
-        x = x.view(-1, 16 * 4 * 4)  # Flatten the tensor
+        x = self.avg2(x)
+        x = x.view(-1, 16 * 5 * 5)  
         x = nn.functional.relu(self.fc1(x))
         x = nn.functional.relu(self.fc2(x))
-        x = self.fc3(x)  # No activation as it will be handled by the loss function (e.g., CrossEntropyLoss)
+        x = self.fc3(x)  
         return x
