fa30b24d1ab8899792ce98a4514e1c2c6e7c6868
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2023-11-16 20:55:48 +0700

    Update googlenet.py

--- model/googlenet.py
+++ model/googlenet.py
@@ -6,32 +6,26 @@ class Inception(nn.Module):
     def __init__(self, in_planes, n1x1, n3x3red, n3x3, n5x5red, n5x5, pool_planes):
         super(Inception, self).__init__()
         # 1x1 conv branch
-        self.b1 = nn.Sequential(
-            nn.Conv2d(in_planes, n1x1, kernel_size=1),
-            nn.ReLU(True)
-        )
+        self.b1 = nn.Conv2d(in_planes, n1x1, kernel_size=1),
 
         # 1x1 conv -> 3x3 conv branch
         self.b2 = nn.Sequential(
             nn.Conv2d(in_planes, n3x3red, kernel_size=1),
             nn.ReLU(True),
             nn.Conv2d(n3x3red, n3x3, kernel_size=3, padding=1),
-            nn.ReLU(True)
         )
 
         # 1x1 conv -> 5x5 conv branch
         self.b3 = nn.Sequential(
             nn.Conv2d(in_planes, n5x5red, kernel_size=1),
             nn.ReLU(True),
-            nn.Conv2d(n5x5red, n5x5, kernel_size=5, padding=2),
-            nn.ReLU(True)
+            nn.Conv2d(n5x5red, n5x5, kernel_size=5, padding=2)
         )
 
         # 3x3 pool -> 1x1 conv branch
         self.b4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(in_planes, pool_planes, kernel_size=1),
-            nn.ReLU(True)
+            nn.Conv2d(in_planes, pool_planes, kernel_size=1)
         )
 
     def forward(self, x):
@@ -48,44 +42,47 @@ class GoogLeNet(nn.Module):
         self.conv1 = nn.Conv2d(config['input_channel'], 64, kernel_size=7, stride=2, padding=3)
         self.conv2 = nn.Conv2d(64, 64, kernel_size=1)
         self.conv3 = nn.Conv2d(64, 192, kernel_size=3, padding=1)
-        self.inception = Inception(192, 64, 96, 128, 16, 32, 32)
-        self.relu1 = nn.ReLU(True)
-        self.relu2 = nn.ReLU(True)
-        self.relu3 = nn.ReLU(True)
-        self.relu4 = nn.ReLU(True)
-        self.relu5 = nn.ReLU(True)
-        self.relu6 = nn.ReLU(True)
+        elf.inception3a = InceptionModule(192, 64, 96, 128, 16, 32, 32)
+        self.inception3b = InceptionModule(256, 128, 128, 192, 32, 96, 64)
+        self.inception4a = InceptionModule(480, 192, 96, 208, 16, 48, 64)
+        self.inception4b = InceptionModule(512, 160, 112, 224, 24, 64, 64)
+        self.inception4c = InceptionModule(512, 128, 128, 256, 24, 64, 64)
+        self.inception4d = InceptionModule(512, 112, 144, 288, 32, 64, 64)
+        self.inception4e = InceptionModule(528, 256, 160, 320, 32, 128, 128)
+        self.inception5a = InceptionModule(832, 256, 160, 320, 32, 128, 128)
+        self.inception5b = InceptionModule(832, 384, 192, 384, 48, 128, 128)
+        self.relu = nn.ReLU(True)
         self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
-        self.maxpool4 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.maxpool4 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.avg = nn.AvgPool2d(kernel_size=(1, 1))
         self.fc = nn.Linear(1024, config['num_classes'])
         
     def forward(self, x):
-        x = self.relu1(self.conv1(x))
+        x = self.relu(self.conv1(x))
         x = self.maxpool1(x)
-        x = self.relu2(self.conv2(x))
-        x = self.relu3(self.conv3(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
         x = self.maxpool2(x)
         
-        x = self.inception(x)
-        x = self.inception(x)
+        x = self.inception3a(x)
+        x = self.inception3b(x)
         
-        x = self.maxpool3(self.relu4(x))
+        x = self.maxpool3(x)
         
-        x = self.inception(x)
-        x = self.inception(x)
-        x = self.inception(x)
-        x = self.inception(x)
-        x = self.inception(x)
+        x = self.inception4a(x)
+        x = self.inception4b(x)
+        x = self.inception4c(x)
+        x = self.inception4d(x)
+        x = self.inception4e(x)
         
-        x = self.maxpool3(self.relu5(x))  
+        x = self.maxpool4(x)
         
-        x = self.inception(x)
-        x = self.inception(x)
+        x = self.inception5a(x)
+        x = self.inception5b(x)
         
-        x = self.avg(self.relu6(x))
+        x = self.avg(self.relu(x))
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
