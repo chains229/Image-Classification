4195d4737e1ab44c5a8a5d1f310e71c0402556f4
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2023-11-17 00:38:42 +0700

    Update googlenet.py

--- model/googlenet.py
+++ model/googlenet.py
@@ -6,7 +6,7 @@ class Inception(nn.Module):
     def __init__(self, in_planes, n1x1, n3x3red, n3x3, n5x5red, n5x5, pool_planes):
         super(Inception, self).__init__()
         # 1x1 conv branch
-        self.b1 = nn.Sequetial(
+        self.b1 = nn.Sequential(
             nn.Conv2d(in_planes, n1x1, kernel_size=1),
             nn.BatchNorm2d(n1x1)
         )
