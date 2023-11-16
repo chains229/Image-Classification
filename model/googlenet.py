b7cbb20bcfb33a30a898d985fc502ba64cfdeaf3
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2023-11-16 20:40:03 +0700

    Update googlenet.py

--- model/googlenet.py
+++ model/googlenet.py
@@ -6,26 +6,32 @@ class Inception(nn.Module):
     def __init__(self, in_planes, n1x1, n3x3red, n3x3, n5x5red, n5x5, pool_planes):
         super(Inception, self).__init__()
         # 1x1 conv branch
-        self.b1 = nn.Conv2d(in_planes, n1x1, kernel_size=1)
+        self.b1 = nn.Sequential(
+            nn.Conv2d(in_planes, n1x1, kernel_size=1),
+            nn.ReLU(True)
+        )
 
         # 1x1 conv -> 3x3 conv branch
         self.b2 = nn.Sequential(
             nn.Conv2d(in_planes, n3x3red, kernel_size=1),
             nn.ReLU(True),
-            nn.Conv2d(n3x3red, n3x3, kernel_size=3, padding=1)
+            nn.Conv2d(n3x3red, n3x3, kernel_size=3, padding=1),
+            nn.ReLU(True)
         )
 
         # 1x1 conv -> 5x5 conv branch
         self.b3 = nn.Sequential(
             nn.Conv2d(in_planes, n5x5red, kernel_size=1),
             nn.ReLU(True),
-            nn.Conv2d(n5x5red, n5x5, kernel_size=5, padding=2)
+            nn.Conv2d(n5x5red, n5x5, kernel_size=5, padding=2),
+            nn.ReLU(True)
         )
 
         # 3x3 pool -> 1x1 conv branch
         self.b4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(in_planes, pool_planes, kernel_size=1)
+            nn.Conv2d(in_planes, pool_planes, kernel_size=1),
+            nn.ReLU(True)
         )
 
     def forward(self, x):
