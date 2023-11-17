9b7cb8c1bfb7249690957423f87f4bb98ce41fdf
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2023-11-17 22:10:29 +0700

    Update resnet18.py

--- model/resnet18.py
+++ model/resnet18.py
@@ -27,19 +27,22 @@ class ResidualBlock(nn.Module):
         out = F.relu(out)
         return out
 
+
 class ResNet(nn.Module):
     def __init__(self, ResidualBlock, config):
         super(ResNet, self).__init__()
         self.inchannel = 64
         self.conv1 = nn.Sequential(
-            nn.Conv2d(config['input_channel'], 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
+            nn.Conv2d(config['input_channel'], 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64)
         )
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.relu = nn.ReLU()
         self.layer1 = self.make_layer(ResidualBlock, 64,  2, stride=1)
         self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride=2)
         self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride=2)
         self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride=2)
+        self.avg = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512, config['num_classes'])
 
     def make_layer(self, block, channels, num_blocks, stride):
@@ -51,15 +54,16 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
+        x = self.relu(self.conv1(x))
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avg(self.relu(x))
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
 
 
 def ResNet_18(config):
