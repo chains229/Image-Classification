ac5fc5adc94889fd4caaa8f2c0c9d1fd08e4138a
Minh Nguyen <nguyenhuutuanminhkh@gmail.com>
2023-12-03 01:06:06 +0700

    Update resnet50.py

--- model/resnet50.py
+++ model/resnet50.py
@@ -1,5 +1,19 @@
 from transformers import ResNetForImageClassification
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
 
-def ResNet_50(config):
-    model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
-    return model
+class ResNet_50(nn.Module):
+    def __init__(self, config):
+        super(ResNet50_Model, self).__init__()
+        self.num_classes = config['num_classes']
+        self.resnet_50 = models.resnet50(pretrained=True)
+        for param in self.resnet_50.parameters():
+            param.requires_grad=False
+        self.resnet_50.fc = nn.Linear(self.resnet_50.fc.in_features, self.num_classes)
+        
+    def forward(self, x):
+        x = self.resnet_50(x)
+        x = F.softmax(x, dim=-1)
+        return x
